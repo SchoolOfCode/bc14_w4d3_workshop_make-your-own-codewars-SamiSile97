7b20f7909cae04bc4a40219c9c75918960eb07fc
samisile <samisile97@gmail.com>
2023-04-09 15:31:08 +0100

    pizza comparison kata

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 /* 
 👉 Write your kata here!
+Pizza size comparison
+
+You go to your favourite pizza store and see a new offer: 1x 14inch Pizza or 2x 10inch Pizza for £10. You want to find out which of the two 
+options is better value for money.
+
+Write a function that takes in a value for inches and converts it to a total area in cm^2 so that you can compare the different offers.
+
+Please note that 1 inch = 2.54cm
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function pizzaToCmSq(inches) {
+    //👉  Write your code here...
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,11 @@
 //👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+import { pizzaToCmSq } from "./main.js"
+
+test("When 10 inch pizza is converted to cm^2", () => {
+    expect(pizzaToCmSq(10)).toBeCloseTo(506.7, 1)
+ })
+
+test("When 14 inch pizza is converted to cm^2", () => {
+    expect(pizzaToCmSq(14)).toBeCloseTo(993.1, 1)
+ })
